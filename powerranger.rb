f4d1873708ea7fbac6751bb6d93d4bec9ca8b322
MakerSquare Student <students@themakersquare.com>
2013-11-18 22:02:24 -0600

    I added the powerranger class and the evilninja class

--- powerranger.rb
+++ powerranger.rb
@@ -1,5 +1,5 @@
 class Person
-	attr_accessor :name, :cafeine_level,
+	attr_accessor :name, :cafeine_level
 
 	def initialize(name)
 		@cafeine_level = 100
@@ -7,7 +7,7 @@ class Person
 	end
 
 	def run
-		puts "I am running."
+		puts "sound of someone running."
 		@cafeine_level -= 15
 	end
 
@@ -22,8 +22,68 @@ class Person
 	end
 end
 
-class PowerRanger
+class PowerRanger < Person
+	attr_accessor :strength, :color, :cafeine_level, :name
+
+	def initialize(strength = 100, color, name)
+		@strength = strength
+		@color = color
+		@cafeine_level = 300
+	end
+
+	def punch(person)
+		if self.strength > 5
+			puts 2.times "#{person.scream}: #{person.run}."
+			person.cafeine_level -= 20
+			@strength -= 5
+		else
+			puts "#{person.scream}: #{person.run}."
+			person.cafeine_level -= 5
+			@strength -= 2
+		end
+	end
+
+	def rest(time_in_minutes)
+		@strength += (time_in_minutes * 2)
+	end
+
+	def use_megazord(person)
+		if self.strength > 50000
+			puts 2.times "#{person.scream}: #{person.run}."
+			person.cafeine_level -= 10000
+			@strength -= 5000
+		else
+			puts "I'm too week"
+			person.cafeine_level += 20
+			@strength -= 50
+		end
+	end
+
 end
 
-class EvilNinja
+class EvilNinja < Person
+	attr_accessor :strength, :evilness, :cafeine_level, :name
+
+	def initialize(cafeine_level, strength = 200, name)
+		@strength = strength
+		@evilness = 300
+		@cafeine_level = cafeine_level
+		@name = name
+	end
+
+	def cause_mayhem(person)
+		person.cafeine_level = 0
 end
+
+bill = Person.new("Bill")
+ted = Person.new("Ted")
+orange = PowerRanger.new(6000, "orange", "orange")
+blue = PowerRanger.new(55, "blue", "blue")
+humpdy = EvilNinja.new(3000, "humpdy")
+dumpdy = EvilNinja.new(25, "dumpdy")
+
+bill.run
+orange.punch(bill)
+humpdy.cause_mayhem(blue)
+
+
