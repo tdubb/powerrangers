b67be79fc91e906b4ba3490c59a34464f8a6c943
MakerSquare Student <students@themakersquare.com>
2013-11-18 21:30:06 -0600

    Initial commit, created Person class and a few methods with a class variable of cafeine_level

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,25 @@
 class Person
+	attr_accessor :name, :cafeine_level,
+
+	def initialize(name)
+		@cafeine_level = 100
+		@name = name
+	end
+
+	def run
+		puts "I am running."
+		@cafeine_level -= 15
+	end
+
+	def scream
+		puts "Ahhhhhhhhh"
+		@cafeine_level -= 5
+	end
+
+	def drink_coffee(x_oz)
+		puts "I feel great!"
+		@cafeine_level += 20
+	end
 end
 
 class PowerRanger
